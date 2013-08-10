bd8b2168d4c294ea966bb5de78b3c90e08809133
Kirill Goldshtein <goldshtein.kirill@gmail.com>
2013-08-10 23:06:21 +0400

    parse args

--- src/haskell/main/Hint/Executable/Hint.hs
+++ src/haskell/main/Hint/Executable/Hint.hs
@@ -1,5 +1,22 @@
+{-# LANGUAGE DeriveDataTypeable #-}
 module Main (main) where
+
+import System.Console.CmdArgs
+
+data Arguments = Arguments
+    {
+        filename :: [String] -- list of filenames or "-" or empty
+    } deriving (Show, Data, Typeable)
+
+
+description :: Arguments
+description = Arguments{filename = [] &= args &= typ "FILE"}
+    &= summary "hint v0.1.0.0"
+    &= program "hint"
+    &= helpArg [explicit, name "h", name "help"]
+
 
 main :: IO ()
 main = do
-    putStrLn "Hello, world!"
+    arguments <- cmdArgs description
+    print arguments
